441532c0e19433c3fb52ee71eeb0313a9b0703bc
cris93g <37388115+cris93g@users.noreply.github.com>
2019-08-21 02:53:58 -0500

    still trying to deply

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
-require("dotenv").config()
-const express= require("express")
-const massive = require("massive")
-const cors = require("cors")
-const {json} = require("body-parser")
-routes= require("./routes/routes")
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const cors = require("cors");
+const { json } = require("body-parser");
+routes = require("./routes/routes");
 
-const app = express()
+const app = express();
 const port = process.env.port || 3001;
 
-app.use(cors())
-app.use(json())
+app.use(cors());
+app.use(json());
 
-routes(app)
-app.listen(port,()=>{
-    console.log(`app is listening on port ${port}`)
-})
+routes(app);
+
+app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
+
+app.listen(port, () => {
+	console.log(`app is listening on port ${port}`);
+});
