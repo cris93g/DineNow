c36969a89179afb215ff224bd4ba7eb14f995980
cris93g <37388115+cris93g@users.noreply.github.com>
2019-08-22 01:54:38 -0500

    checking if it works

--- server/index.js
+++ server/index.js
@@ -3,15 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const { json } = require("body-parser");
 routes = require("./routes/routes");
-
 const app = express();
-const port = process.env.port || 3001;
-app.use(express.static(__dirname+ '/../build'))
 app.use(cors());
 app.use(json());
+app.use(express.static(__dirname+ '/../build'))
+
 
 routes(app);
-
+const port = process.env.port || 3001;
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, ">./build/index.html"));
   });
